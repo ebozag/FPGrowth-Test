2df61f9d31caa12a6c06214575486ec212f25622
Edwin F. Boza <ebozag@gmail.com>
2016-12-06 10:55:51 -0500

    Fixed scriptChangeToFPGrowthInputFormat.sh to avoid manually setting the ID column inside the script.

--- scriptChangeToFPGrowthInputFormat.sh
+++ scriptChangeToFPGrowthInputFormat.sh
@@ -11,6 +11,6 @@ paste -d "," $trace-uniqueIDs.txt newIDs.temp > $trace-pattern
 #http://unix.stackexchange.com/questions/126485/replacing-the-values-in-one-file-with-the-values-in-another-file-in-bash
 awk -F',|, ' 'NR==FNR{a[$1]=$2} NR>FNR{$1=a[$1];print}' OFS=',' "$trace-pattern" "$trace-onlyIDs.txt" > $trace-onlyIDs-transformed.txt
 ### For this line, "NR>FNR{$2=a[$2]", needs to manually use the column with IDs on the trace.
-awk -F',|, ' 'NR==FNR{a[$1]=$2} NR>FNR{$2=a[$2];print}' OFS=',' "$trace-pattern" "$trace" > $trace-transformed.txt
+awk -v column="$column" -F',|, ' 'NR==FNR{a[$1]=$2} NR>FNR{$column=a[$column];print}' OFS=',' "$trace-pattern" "$trace" > $trace-transformed.txt
 rm -f $trace-onlyIDs.txt $trace-uniqueIDs.txt newIDs.temp
 
